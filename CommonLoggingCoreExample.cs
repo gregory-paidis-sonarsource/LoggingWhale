55805772d02fa9b7ac9a5372060c24dd9a7a3e8c
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-02-21 16:00:57 +0100

    Add Common.Logging

---
+++ CommonLoggingCoreExample.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using Common.Logging;
+
+namespace Logging;
+
+public class CommonLoggingCoreExample
+{
+    public void Logging(ILog logger, Exception e)
+    {
+        logger.Debug("Message");
+        logger.Debug("Message", e);
+        logger.Debug(_ => { });
+        logger.Debug(_ => { }, e);
+        logger.Debug(CultureInfo.CurrentCulture, _ => { });
+        logger.Debug(CultureInfo.CurrentCulture, _ => { }, e);
+        logger.DebugFormat("Message");
+        logger.DebugFormat("Message", e);
+        logger.DebugFormat(CultureInfo.CurrentCulture, "Message");
+        logger.DebugFormat(CultureInfo.CurrentCulture, "Message", e);
+
+        logger.Error("Message");
+        logger.Error("Message", e);
+
+        logger.Fatal("Message");
+        logger.Fatal("Message", e);
+
+        logger.Info("Message");
+        logger.Info("Message", e);
+
+        logger.Trace("Message");
+        logger.Trace("Message", e);
+
+        logger.Warn("Message");
+        logger.Warn("Message", e);
+    }
+}
